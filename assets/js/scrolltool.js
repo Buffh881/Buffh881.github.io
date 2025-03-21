1e692229510104ecbdfe007e68f76d2be773281a
anastasiya <anastasiyapogudina08.gmail.com>
2025-03-21 16:08:06 +0300

    first commit

---
+++ assets/js/scrolltool.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(event) {
+        event.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').animate({
+            scrollTop: $target.offset().top
+        }, 800, 'swing');
+    });
+});
+
